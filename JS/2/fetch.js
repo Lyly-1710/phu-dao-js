8a7f758dfd02a7c13d09b917a29895b6b5c39c8b
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-05-26 13:05:30 +0700

    baitap day2

--- JS/2/fetch.js
+++ JS/2/fetch.js
@@ -11,11 +11,11 @@ fetch(api)
   .then(data => {
     const items = data.data.items;
 
-    // Efficiently find items with highest variant price using reduce:
+    
     const results = items.reduce((acc, item) => {
       const highestVariant = item.variants.reduce((maxVariant, variant) => {
         return variant.price > maxVariant.price ? variant : maxVariant;
-      }, item.variants[0]); // Initialize with first variant
+      }, item.variants[0]); 
 
       acc.push({
         id: highestVariant.id,
@@ -25,10 +25,9 @@ fetch(api)
       return acc;
     }, []);
 
-    // Find the single item with the highest price among all highest variants:
     const singleHighestPrice = results.reduce((max, current) => {
       return max.price > current.price ? max : current;
-    }, results[0]); // Initialize with first item
+    }, results[0]); 
 
     // Find the variant with the matching ID within the highest-priced item:
     const matchingVariant = items.find(
@@ -36,19 +35,40 @@ fetch(api)
     );
 
     if (matchingVariant) {
-      console.log(`${matchingVariant.title}  
-            Price: $${matchingVariant.variants[0].price} USD  
-            `); // Access price from the first element of variants array
+      // console.log(`${matchingVariant.title}  
+      //       Price: $${matchingVariant.variants[0].price} USD  
+      //       `); // Access price from the first element of variants array
     } else {
       console.log("No matching variant found."); // Handle potential case where no matching ID exists
     }
+
+    //cau 2
     const TestBundle = items.find(
       (item) => item.title === "Test bundle"
     );
     const total = TestBundle.variants.reduce((acc, curr) =>{
       return acc + curr.price
     }, 0)
-    console.log("Test Bundle")
-    console.log(`Teotal price: ${total}USD`)
+    // console.log("Test Bundle")
+    // console.log(`Teotal price: ${total}USD`)
+
+    //cau 3
+    const soldOUt = items.map((item)=>{
+      if(item.variants.every((variant) => variant.available === 0))
+      {
+        //console.log(item.title)
+      }
+    },null)
+    const size = items.map((item) => Object.values(item.options))
+    console.log(size)
+    //cau 4
+    
+  
   })
   .catch(err => console.error(err));
+
+  // function check(size, color)
+  // {
+  //   let arr = document.querySelector('#input4').value.split('|');
+  //   console.log(size)
+  // }
